11940bb22eb2ea0f1177309b973d0bf088eb7686
yxzhin <yxzhin@gmail.com>
2026-06-23 11:53:18 +0200

    implement login view model

---
+++ filmoviCrud/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace filmoviCrud.ViewModels
+{
+    public class LoginViewModel
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
