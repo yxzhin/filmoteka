ecbb0dc815b17db0456641c8a1be0a54991ee700
yxzhin <yxzhin@gmail.com>
2026-06-23 11:47:06 +0200

    implement register view model

---
+++ filmoviCrud/Models/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace filmoviCrud.Models
+{
+    public class RegisterViewModel
+    {
+        public required string Ime { get; set; }
+        public required string Prezime { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
