0047b270a3f0f547f6e2f94947a7acf6f2a3f5d2
yxzhin <yxzhin@gmail.com>
2026-06-25 10:21:54 +0200

    implement auth controller

---
+++ src/backend/filmotekaAPI/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+using filmotekaAPI.DTOs;
+using filmotekaAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace filmotekaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(KorisnikService korisnikService)
+        : ControllerBase
+    {
+        private readonly KorisnikService _korisnikService = korisnikService;
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterInputDTO registerInputDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            BaseResponseDTO registerResponseDTO = await _korisnikService
+                .Register(registerInputDTO.Ime, registerInputDTO.Prezime,
+                registerInputDTO.Email, registerInputDTO.Password);
+            return new JsonResult(registerResponseDTO);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginInputDTO loginInputDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            LoginResponseDTO loginResponseDTO = await _korisnikService
+                .Login(loginInputDTO.Email, loginInputDTO.Password);
+            return new JsonResult(loginResponseDTO);
+        }
+    }
+}
