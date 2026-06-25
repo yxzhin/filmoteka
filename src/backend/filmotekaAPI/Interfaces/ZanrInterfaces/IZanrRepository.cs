b3b2433a4d788e89278c2fc832f21b35d2a73e1f
yxzhin <yxzhin@gmail.com>
2026-06-25 11:13:08 +0200

    implement zanr repository interface

---
+++ src/backend/filmotekaAPI/Interfaces/ZanrInterfaces/IZanrRepository.cs
@@ -0,0 +1,13 @@
+using filmotekaAPI.Models;
+
+namespace filmotekaAPI.Interfaces.ZanrInterfaces
+{
+    public interface IZanrRepository : IRepository<Zanr>
+    {
+        new Task<Zanr?> GetById(int id);
+        new Task<List<Zanr>> GetMany(int offset = 0, int limit = 10);
+        Task<Zanr?> GetByEmail(string email);
+        new Task Save(Zanr entity);
+        new Task Delete(Zanr entity);
+    }
+}
