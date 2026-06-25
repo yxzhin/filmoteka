9098c4579e21228fd1b14b940e7cdcbb275fa853
yxzhin <yxzhin@gmail.com>
2026-06-25 11:27:29 +0200

    implement zanr service interface

---
+++ src/backend/filmotekaAPI/Interfaces/ZanrInterfaces/IZanrService.cs
@@ -0,0 +1,15 @@
+using filmotekaAPI.DTOs;
+using filmotekaAPI.DTOs.ZanrDTOs;
+
+namespace filmotekaAPI.Interfaces.ZanrInterfaces
+{
+    public interface IZanrService
+    {
+        Task<ZanrGetOneResponseDTO> GetById(int id);
+        Task<ZanrGetManyResponseDTO> GetMany(int offset = 0, int limit = 10);
+        Task<ZanrGetOneResponseDTO> GetByName(string name);
+        Task<BaseResponseDTO> Create(string name);
+        Task<BaseResponseDTO> Update(int id, string newName);
+        Task<BaseResponseDTO> Delete(int id);
+    }
+}
